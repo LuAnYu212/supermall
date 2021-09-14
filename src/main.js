d1e2e12b0139150d33f64aa64f7a42bc5dd7a138
lay <1518170682@qq.com>
2021-09-14 23:55:43 +0800

    打包

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import VueLazyLoad from 'vue-lazyload';
+
 import toast from 'components/common/toast'
 
 Vue.config.productionTip = false
 
 Vue.use(toast)
+
+Vue.use(VueLazyLoad)
 
 new Vue({
   render: h => h(App),
